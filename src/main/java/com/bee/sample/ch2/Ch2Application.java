397d9c5a30a4056455de21b71c742c47d382f1a6
xuexionghui <413669152@qq.com>
2018-09-12 10:16:42 +0800

    启动类启动的时候，不再出现spring启动的图案

--- src/main/java/com/bee/sample/ch2/Ch2Application.java
+++ src/main/java/com/bee/sample/ch2/Ch2Application.java
@@ -1,14 +1,17 @@
 package com.bee.sample.ch2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ch2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ch2Application.class, args);
-
+		//SpringApplication.run(Ch2Application.class, args);
+        SpringApplication application = new SpringApplication(Ch2Application.class);
+        application.setBannerMode(Mode.OFF);
+        application.run(args);
 	}
 
 }
